079d12d138f2fcc45ff634cdfe523fb1f441c454
rohithP172 <149103905+rohithP172@users.noreply.github.com>
2026-07-17 21:18:36 -0500

    Update helloWorldWelcome function to render index.html

--- HelloWorldProject/helloWorldWelcomeProgram.py
+++ HelloWorldProject/helloWorldWelcomeProgram.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloWorldWelcome():
-    # return "Hello, World! Welcome to the Artificial Intelligence and Human Computer Interaction Course."
+    
     return render_template('index.html')
 
 
